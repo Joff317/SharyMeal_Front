7490a33d434ed817da034acb058606fcd5829a57
foucauld <foucauldbeaumont@yahoo.fr>
2022-09-14 17:10:14 +0200

    fixing bugs style

--- src/components/meals/mealCard/MealAvatar.js
+++ src/components/meals/mealCard/MealAvatar.js
@@ -1,31 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import { API } from '../../../utils/variables';
-import avatarDefault from './avatar-default.png'
+import React, { useState, useEffect } from "react";
+import { API } from "../../../utils/variables";
+import avatarDefault from "./avatar-default.png";
 
-function MealAvatar({host}) {
+function MealAvatar({ host }) {
+  const [hostDetails, setHostDetails] = useState(null);
 
-    const [ hostDetails, setHostDetails ] = useState(null)
+  useEffect(() => {
+    fetch(API + `/user_detail/${host.id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("data MealAvatar => ", data);
+        setHostDetails(data);
+      });
+  }, []);
 
-    useEffect(() => {
-        fetch(API + `/user_detail/${host.id}`)
-        .then(res => res.json())
-        .then(data => {
-            console.log('data MealAvatar => ', data);
-            setHostDetails(data);
-        })
-    }, [])
-
-
-    return (
-        <div>
-            {
-                hostDetails && hostDetails.avatar_url ?
-                        <img src={hostDetails.avatar_url} alt="host"/>
-                        :
-                        <img src={avatarDefault} alt="host" className='w-24'/>
-            }
-        </div>
-    );
+  return (
+    <>
+      {hostDetails && hostDetails.avatar_url ? (
+        <img src={hostDetails.avatar_url} alt="host" className="w-6" />
+      ) : (
+        <img src={avatarDefault} alt="host" className="w-6" />
+      )}
+    </>
+  );
 }
 
 export default MealAvatar;
