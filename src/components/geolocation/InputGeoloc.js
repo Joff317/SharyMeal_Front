45a1484f017ba24538a97c94ec7422b6c2e4d07a
foucauld <foucauldbeaumont@yahoo.fr>
2022-09-14 13:36:24 +0200

    adding date picker

--- src/components/geolocation/InputGeoloc.js
+++ src/components/geolocation/InputGeoloc.js
@@ -1,25 +1,37 @@
 import Button from "../actions/Button";
 import Arrow from "../../icons/Arrow";
-import { useForm } from "react-hook-form";
-import { useState } from "react";
+import { useState, forwardRef } from "react";
 import Autocompletion from "./Autocompletion";
 import { useAtomValue, useSetAtom } from "jotai";
 import { inputDataAtom } from "../../atoms/inputData";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function InputGeoloc() {
-  const { register, handleSubmit } = useForm();
   const [autocompleteVisible, setAutocompleteVisible] = useState(false);
   const [autocomplete, setAutocomplete] = useState(false);
   const [cityInfo, setCityInfo] = useState();
   const setInputData = useSetAtom(inputDataAtom);
   const inputDataValue = useAtomValue(inputDataAtom);
+  const [startDate, setStartDate] = useState(new Date());
 
-  function onSubmit(data) {
+  function onSubmit(e) {
+    e.preventDefault();
     setInputData({
-      city: cityInfo.city,
-      date: data.date,
+      city: cityInfo && cityInfo.city,
+      date: startDate,
     });
   }
+
+  const CustomInput = forwardRef(({ value, onClick }, ref) => (
+    <button
+      className=" font-light-font text-sm mr-4 mb-0.5"
+      onClick={onClick}
+      ref={ref}
+    >
+      {value}
+    </button>
+  ));
 
   console.log(inputDataValue);
 
@@ -41,7 +53,7 @@ function InputGeoloc() {
 
   return (
     <div className="rounded-full bg-white w-fit py-2 pl-6 pr-2 relative">
-      <form onSubmit={handleSubmit(onSubmit)} className={`flex items-center`}>
+      <form onSubmit={(e) => onSubmit(e)} className={`flex items-center`}>
         <label className="text-sm"> Ta ville :</label>
         <input
           className="pl-2 mt-0.5 text-sm font-light-font focus:visible "
@@ -62,12 +74,15 @@ function InputGeoloc() {
               ))}
           </div>
         )}
-        <label className="text-sm"> Quand ? </label>
-        <input
-          className="text-sm pl-2 mt-0.5 font-light-font"
-          type="text"
-          {...register("date")}
-        />
+        <div className="flex items-center gap-2">
+          <label className="text-sm min-w-[60px]"> Quand ?</label>
+
+          <DatePicker
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            customInput={<CustomInput />}
+          />
+        </div>
 
         <button type="submit">
           <Button showIcon={true} icon={<Arrow />} />
