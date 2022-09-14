1172065d4a01fc4dacb387493a2dd1afa668ee77
Joff317 <j.freixanet@hotmail.com>
2022-09-14 11:37:42 +0200

    navbar_design_orga

--- src/components/actions/Button.js
+++ src/components/actions/Button.js
@@ -1,10 +1,10 @@
-function Button({ children, icon, showText }) {
+function Button({ children, icon, showText, showIcon }) {
   return (
     <div
       className={`flex font-medium-font text-sm gap-2 items-center p-2.5 bg-pink w-fit rounded-full`}
     >
       {showText && <p> {children} </p>}
-      {icon}
+      {showIcon && icon}
     </div>
   );
 }
