02f5803512fa0b4abd712ee7b738d9ea6edad8f1
foucauld <foucauldbeaumont@yahoo.fr>
2022-09-16 11:31:43 +0200

    create meal creation

--- src/components/meals/mealDetails/MealDetailsImages.js
+++ src/components/meals/mealDetails/MealDetailsImages.js
@@ -12,49 +12,37 @@ const MealDetailsImages = ({ meal }) => {
     <div className="meal-detail-left-container">
       <img
         className="box-1"
-        src={
-          meal.image_urls.length > 0 ? meal.image_urls[0] : MealDetailsImage1
-        }
+        src={meal.image_urls ? meal.image_urls[0] : MealDetailsImage1}
         alt=""
       />
 
       <img
         className="box-2"
-        src={
-          meal.image_urls.length > 1 ? meal.image_urls[1] : MealDetailsImage2
-        }
+        src={meal.image_urls ? meal.image_urls[1] : MealDetailsImage2}
         alt=""
       />
 
       <img
         className="box-3"
-        src={
-          meal.image_urls.length > 2 ? meal.image_urls[2] : MealDetailsImage3
-        }
+        src={meal.image_urls ? meal.image_urls[2] : MealDetailsImage3}
         alt=""
       />
 
       <img
         className="box-4"
-        src={
-          meal.image_urls.length > 3 ? meal.image_urls[3] : MealDetailsImage4
-        }
+        src={meal.image_urls ? meal.image_urls[3] : MealDetailsImage4}
         alt=""
       />
 
       <img
         className="box-5"
-        src={
-          meal.image_urls.length > 4 ? meal.image_urls[4] : MealDetailsImage5
-        }
+        src={meal.image_urls ? meal.image_urls[4] : MealDetailsImage5}
         alt=""
       />
 
       <img
         className="box-6"
-        src={
-          meal.image_urls.length > 5 ? meal.image_urls[5] : MealDetailsImage6
-        }
+        src={meal.image_urls ? meal.image_urls[5] : MealDetailsImage6}
         alt=""
       />
     </div>
