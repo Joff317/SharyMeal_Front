8342782b77f66984c9b7fe7403bf2a05aecd94d3
Ju777 <julien.tamil@gmail.com>
2022-09-16 18:01:20 +0200

    Update URLs in utils.

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,12 +1,12 @@
-// export let API;
+export let API;
 
-// if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-//     // dev code
-//     API = "http://localhost:3000/";
-// } else {
-//     // production code
-//     API = "https://sharymeal-backend.herokuapp.com/";
-// }
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    // dev code
+    API = "http://localhost:3000/";
+} else {
+    // production code
+    API = "https://sharymeal-backend.herokuapp.com/";
+}
 
 // export const API = "https://sharymeal-backend.herokuapp.com/"
-export const API = "http://localhost:3000/"
+// export const API = "http://localhost:3000/"
