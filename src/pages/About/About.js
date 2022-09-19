d91743fcd53e7e279c2a20c8e4451b46a7eaf379
Joff317 <j.freixanet@hotmail.com>
2022-09-19 11:05:35 +0200

    middle container about us

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -5,7 +5,10 @@ import HeroTitle from "../../components/titles/HeroTitle";
 import Button from "../../components/actions/Button";
 import image3 from "../../assets/images/imagehome3.jpeg";
 import image1 from "../../assets/images/imagehome1.jpeg";
+import aboutImage from "../../assets/images/about-image.jpeg";
 import "./About.scss";
+import SectionTitle from "../../components/titles/SectionTitle";
+import SubsectionTitle from "../../components/titles/SubsectionTitle";
 
 function About(props) {
   return (
@@ -30,7 +33,101 @@ function About(props) {
         <div className="top-container-right">
           <div className="img-container">
             <img src={image3} alt="" className="image-3" />
-            <img src={image1} alt="" className="image-1"/>
+            <img src={image1} alt="" className="image-1" />
+          </div>
+        </div>
+      </div>
+
+      <div className="middle-container">
+        <span className="middle-title">
+          <SectionTitle>Comment ça fonctionne ?</SectionTitle>
+        </span>
+        <div className="middle-left-container">
+          <div className="first-double-circle">
+            <div className="circle-1">
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+            </div>
+            <div className="circle-2">
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+            </div>
+          </div>
+          <div className="double-circle">
+            <div className="circle-3">
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+            </div>
+            <div className="circle-4">
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+              <span className="circle"></span>
+            </div>
+            <img src="" alt="" />
+          </div>
+          <div className="square"></div>
+          <img src={aboutImage} alt="" className="about-image" />
+        </div>
+        <div className="middle-container-right">
+          <div className="text-1">
+            <SubsectionTitle>Créer votre annonce</SubsectionTitle>
+            <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima repudiandae animi asperiores magni. Animi, perferendis ipsa vel reprehenderit ullam voluptatibus harum. Ratione iste quibusdam labore quae, asperiores alias dicta id!
+            Soluta aliquid quas quaerat voluptates modi quod, aspernatur deserunt, expedita eaque nesciunt sit exercitationem ut assumenda excepturi explicabo nam ab cupiditate rem nihil in doloribus nostrum. Corporis voluptatum harum quibusdam?
+            </p>
+          </div>
+          <div className="text-2">
+          <SubsectionTitle>Gérer les demande de participations </SubsectionTitle>
+          <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima repudiandae animi asperiores magni. Animi, perferendis ipsa vel reprehenderit ullam voluptatibus harum. Ratione iste quibusdam labore quae, asperiores alias dicta id!
+            Soluta aliquid quas quaerat voluptates modi quod, aspernatur deserunt, expedita eaque nesciunt sit exercitationem ut assumenda excepturi explicabo nam ab cupiditate rem nihil in doloribus nostrum. Corporis voluptatum harum quibusdam?
+          </p>
+          </div>
+          <div className="text-3">
+          <SubsectionTitle>Mangez et profitez !</SubsectionTitle>
+          <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima repudiandae animi asperiores magni. Animi, perferendis ipsa vel reprehenderit ullam voluptatibus harum. Ratione iste quibusdam labore quae, asperiores alias dicta id!
+            Soluta aliquid quas quaerat voluptates modi quod, aspernatur deserunt, expedita eaque nesciunt sit exercitationem ut assumenda excepturi explicabo nam ab cupiditate rem nihil in doloribus nostrum. Corporis voluptatum harum quibusdam?</p>
           </div>
         </div>
       </div>
