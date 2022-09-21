1afd98b2774acaea66c6b6d1a8fa75e909566b40
foucauld <foucauldbeaumont@yahoo.fr>
2022-09-21 22:21:01 +0200

    adding animation on messages

--- src/components/user/Chat/ListMessages.js
+++ src/components/user/Chat/ListMessages.js
@@ -15,7 +15,7 @@ function ListMessages({ listMessages, setCurrentChatterId, currentChatterId }) {
         listMessages.map((listMessage, index) => (
           <p
             key={index}
-            className={` cursor-pointer border-b border-grey-border hover:bg-green_light py-4 mb-2 rounded-lg px-4 relative ${
+            className={`slide cursor-pointer border-b border-grey-border hover:bg-green_light py-4 mb-2 rounded-lg px-4 relative ${
               currentChatterId === listMessage.id &&
               "bg-green_light hover:bg-grey-border"
             }`}
