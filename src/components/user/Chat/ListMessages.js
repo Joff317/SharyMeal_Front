d1523badc3697835b27ba829df43e8947c2f973e
foucauld <foucauldbeaumont@yahoo.fr>
2022-09-22 13:12:41 +0200

    fix issues

--- src/components/user/Chat/ListMessages.js
+++ src/components/user/Chat/ListMessages.js
@@ -7,15 +7,19 @@ function ListMessages({ listMessages, setCurrentChatterId, currentChatterId }) {
   useEffect(() => {
     ScrollReveal().reveal(".slide", slideUpFast);
   }, []);
-  // console.log(listMessages);
+
+  let reversed = [...listMessages].reverse();
+  useEffect(() => {
+    setCurrentChatterId(reversed[0].id);
+  }, []);
 
   return (
     <div className="relative ">
       {listMessages &&
-        listMessages.map((listMessage, index) => (
+        reversed.map((listMessage, index) => (
           <p
             key={index}
-            className={`slide cursor-pointer border-b border-grey-border hover:bg-green_light py-4 mb-2 rounded-lg px-4 relative ${
+            className={`slide cursor-pointer border-b border-grey-border hover:bg-grey-border py-4 mb-2 rounded-lg px-4 relative ${
               currentChatterId === listMessage.id &&
               "bg-green_light hover:bg-grey-border"
             }`}
